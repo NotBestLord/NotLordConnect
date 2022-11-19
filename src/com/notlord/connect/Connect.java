b25f9ace99de7a44e589e0bbcb53f94963c35978
NotBestLord <amir01zx@gmail.com>
2022-11-19 20:55:42 +0200

    2nd

--- src/com/notlord/connect/Connect.java
+++ src/com/notlord/connect/Connect.java
@@ -2,6 +2,8 @@ package com.notlord.connect;
 
 import java.io.*;
 import java.net.*;
+import java.util.Enumeration;
+import java.util.regex.Pattern;
 
 public class Connect {
 	/**
@@ -10,7 +12,7 @@ public class Connect {
 	 * @throws Exception HTTP Request Failed / Reading Request Failed
 	 */
 	public static int generateGameId() throws Exception {
-		URL url = new URL("https://notlord.com/connect/request_host_id/");
+		URL url = new URL("https://notlord.com/connect/request_host_id:"+getPublicIPv4()+"/");
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setRequestMethod("GET");
 		con.connect();
@@ -77,5 +79,34 @@ public class Connect {
 		}
 		con.disconnect();
 		return false;
+	}
+
+	private static String getPublicIPv4() throws SocketException {
+		Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces();
+		String ipToReturn = null;
+		while(e.hasMoreElements())
+		{
+			NetworkInterface n = e.nextElement();
+			Enumeration<InetAddress> ee = n.getInetAddresses();
+			while (ee.hasMoreElements())
+			{
+				InetAddress i = ee.nextElement();
+				String currentAddress = i.getHostAddress();
+				if(!i.isSiteLocalAddress()&&!i.isLoopbackAddress() && validate(currentAddress)){
+					ipToReturn = currentAddress;
+				}else{
+					//System.out.println("Address not validated as public IPv4");
+				}
+
+			}
+		}
+		return ipToReturn;
+	}
+
+	private static final Pattern IPv4RegexPattern = Pattern.compile(
+			"^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+
+	private static boolean validate(final String ip) {
+		return IPv4RegexPattern.matcher(ip).matches();
 	}
 }
